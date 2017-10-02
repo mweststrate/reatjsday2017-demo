1b1facbe09d47451d26584f73c11b82a71169481
Michel Weststrate <mweststrate@gmail.com>
2017-10-02 17:29:48 +0200

    cleaned everything

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,4 +1,4 @@
-import React, { Component, PropTypes } from "react"
+import React, { Component } from "react"
 import classnames from "classnames"
 import TodoTextInput from "./TodoTextInput"
 import { observer } from "mobx-react"
